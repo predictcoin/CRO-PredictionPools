58b7c63761f2b5348cd06afebf4e64787bb04561
nonseodion <nonseifebhor@gmail.com>
2022-04-20 23:37:19 +0100

    deployed to Mainnet

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,28 +1,28 @@
 import { ethers, upgrades } from "hardhat"
 
 async function main() {
-  // We get the contract to deploy
+  // We get the contract to upgrade
   const LoserFarm = await ethers.getContractFactory("LoserPredictionPool");
   const WinnerFarm = await ethers.getContractFactory("WinnerPredictionPool");
-  const winnerFarm = await upgrades.upgradeProxy(process.env.WINNER_POOL_ADDRESS || "",
+  const winnerFarm = await upgrades.upgradeProxy(process.env.WINNERPOOL || "",
     WinnerFarm, {kind: "uups"}
   );
-  // const loserFarm = await upgrades.upgradeProxy(process.env.LOSER_POOL_ADDRESS || "",
-  //   LoserFarm, {kind: "uups"}
-  // );
+  const loserFarm = await upgrades.upgradeProxy(process.env.LOSERPOOL || "",
+    LoserFarm, {kind: "uups"}
+  );
 
-  console.log(process.env.WINNER_POOL_ADDRESS);
-  console.log(process.env.LOSER_POOL_ADDRESS);
+  console.log(process.env.WINNERPOOL);
+  console.log(process.env.LOSERPOOL);
 
   console.log(`WinnerFarm implementation deployed to:${await ethers.provider.getStorageAt(
-    process.env.WINNER_POOL_ADDRESS || "",
+    process.env.WINNERPOOL || "",
     "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
   )}`);
   
-  // console.log(`LoserFarm implementation deployed to:${await ethers.provider.getStorageAt(
-  //   process.env.LOSER_POOL_ADDRESS || "",
-  //   "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
-  // )}`);
+  console.log(`LoserFarm implementation deployed to:${await ethers.provider.getStorageAt(
+    process.env.LOSERPOOL || "",
+    "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+  )}`);
 }
 
 main()
